89ac5abcb51fe4833aaf8b1475b2e5cb2535bd34
Edward Aschan <edward@aschan.org>
2019-03-22 14:35:50 +0100

    Creating initial middleware application

---
+++ src/Aschan.AspNetCore.Http/ClacksMiddleware.cs
@@ -0,0 +1,30 @@
+﻿namespace Aschan.AspNetCore.Http
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using Microsoft.AspNetCore.Http;
+
+    public class ClacksMiddleware
+    {
+        private const string XClacksOverhead = @"X-Clacks-Overhead";
+        private const string GnuTerryPrattchet = @"GNU Terry Pratchett";
+
+        private readonly RequestDelegate _next;
+
+        public ClacksMiddleware(RequestDelegate next)
+        {
+            _next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            if (httpContext==null)
+                throw new ArgumentNullException(nameof(httpContext));
+
+            httpContext.Response.Headers.Add(XClacksOverhead, GnuTerryPrattchet);
+
+            return _next(httpContext);
+        }
+    }
+}
---
+++ src/Aschan.AspNetCore.Http/MiddlewareExtensions.cs
@@ -0,0 +1,26 @@
+﻿namespace Aschan.AspNetCore.Http
+{
+    using System;
+
+    using Microsoft.AspNetCore.Builder;
+
+    /// <summary>
+    /// Extension methods to add capabilities to an HTTP application pipeline.
+    /// </summary>
+    public static class MiddlewareExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="T:Aschan.AspNetCore.Http.ClacksMiddleware"/> to the specified <see cref="T:Microsoft.AspNetCore.Builder.IApplicationBuilder" />, which writes an X-Clacks-Overhead response header to the response.
+        /// </summary>
+        /// <param name="applicationBuilder">The IApplicationBuilder to add the middleware to.</param>
+        /// <returns>A reference to this instance after the operation has completed.</returns>
+        /// <remarks> GNU Terry Pratchett (http://www.gnuterrypratchett.com/).</remarks>
+        public static IApplicationBuilder UseClacks(this IApplicationBuilder applicationBuilder)
+        {
+            if (applicationBuilder == null)
+                throw new ArgumentNullException(nameof(applicationBuilder));
+
+            return applicationBuilder.UseMiddleware<ClacksMiddleware>();
+        }
+    }
+}
